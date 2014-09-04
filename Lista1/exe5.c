3b947de89403d93916befc77f1251cda4b8111b8
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-09-04 11:59:58 -0300

    Parcial

---
+++ Lista1/exe5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int a, b;
+    printf("Digite a:\n");
+    scanf("%d", &a);
+    printf("Digite b:\n");
+    scanf("%d", &b);
+    soma =  a + b;
+    a = soma;
+    printf("%d", a);
+    
+    }
