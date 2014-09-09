ffbf8777f3d3fa9c3ec498053bf75d9769215491
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-09-09 00:12:13 -0300

    Exercise 4

--- Lista1/exe4.c
+++ Lista1/exe4.c
@@ -2,8 +2,9 @@
 
 float a,b;
 float media_simples, media_ponderada;
+int c;
 
-int main(void)
+int main()
 {
    void exibe_dados(float a, float b);
    printf("Digite duas notas:\n");
@@ -16,22 +17,42 @@ int main(void)
 
 void le_notas(float a, float b)
 {
-    return a, b;
+   if(a < 0 ||  b < 0)
+    {
+        printf("Digite valores positivo\n");
+        c = 0;
+    }
+    else if(a > 10 || b > 10 )
+    {
+        printf("Digite valores menores que dez\n");
+        c = 0;
+    }
+    else
+    {
+        printf("\nAs notas sao: %.2f e %.2f\n",a,b);
+        c = 1;
+    };
 }
 
 void calc_media(float a, float b)
 {
-    media_simples = (a + b) / 2;
-    media_ponderada = (a + b*2) / 3;
-    return media_simples, media_ponderada;
+    if(c == 1)
+    {
+        media_simples = (a + b) / 2;
+        media_ponderada = (a + b*2) / 3;
+        return media_simples, media_ponderada;
+    }
+    else
+    {
+        printf("Nao foi possivel calcular as medias\n");
+    }
 }
 
 void exibe_dados(float a, float b)
 {
     void le_notas(float a, float b);
     void calc_media(float a, float b);
-    calc_media(a,b);
     le_notas(a,b);
-    printf("\nAs notas sao: %.2f e %.2f\n",a,b);
+    calc_media(a,b);
     printf("As medias simples e ponderada, respectivamente, sao: %.2f e %.2f",media_simples, media_ponderada);
 }
