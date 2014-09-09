fec5e1b2e7d6241d510b56c25dcc1b1412d0c7fa
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-09-09 09:38:40 -0300

    Exercise 5

--- Lista1/exe5.c
+++ Lista1/exe5.c
@@ -4,20 +4,22 @@ int a,b;
 
 int main(void)
 {
-   void soma(int a, int b);
+   void soma(int *pA, int *pB);
    printf("Digite dois valores:\n");
    scanf("%i", &a);
    scanf("%i", &b);
    printf("Os dois numeros digitados foram: %i e %i\n",a,b);
-   soma(a,b);
+   int *pA = &a;
+   int *pB = &b;
+   soma(pA,pB);
+   printf("Agora a variavel A tem o valor da soma de A e B que eh: %i\n",a);
    return 0;
 
 }
 
-void soma(int a, int b)
+void soma(int *a, int *b)
 {
     int soma;
-    soma = a + b;
-    a = soma;
-    printf("Agora a variavel A tem o valor da soma de A e B: %i e %i\n",a,b);
+    soma = *a + *b;
+    *a = soma;
 }
