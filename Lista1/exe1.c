a678776da71cad8117aaf6a45db7407875926185
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-09-12 22:59:18 -0300

    Exercise 1

--- Lista1/exe1.c
+++ Lista1/exe1.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
-
 int main()
 {
-    int a,b;
-    int troca(int a, int b);
-    printf("Digite dois valores:\n");
-    scanf("%i", &a);
-    scanf("%i", &b);
-    printf("Antes 'a' eh:%i e 'b' eh:%i\n",a,b);
-    troca(a,b);
-    return 0;
-
+int a,b;
+int troca(int *x, int *y);
+printf("Digite dois valores, separados por linha:\n");
+scanf("%i", &a);
+scanf("%i", &b);
+printf("Antes 'a' eh:%i e 'b' eh:%i\n",a,b);
+troca(&a,&b);
+printf("Agora 'a' eh:%i e 'b' eh:%i\n",a,b);
+return 0;
 }
 
-int troca(int x, int y)
+int troca(int *x, int *y)
 {
-    int temporaria;
-    temporaria = x;
-    x = y;
-    y = temporaria;
-    printf("Agora 'a' eh:%i e 'b' eh:%i\n",x,y);
-
+int temporaria = *x;
+*x = *y;
+*y = temporaria;
 }
