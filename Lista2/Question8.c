2b126e4140389231db53bf2e0a06125d6d59abed
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-09-17 21:29:40 -0300

    Questao 8

--- Lista2/Question8.c
+++ Lista2/Question8.c
@@ -1,21 +1,3 @@
-#include <stdio.h>
-int main()
-{
-    int N,i,maximo,minimo;
-    void maximoMinimo(int *v, int N, int *maximo, int *minimo);
-    printf("Digite o numero de elementos de seu vetor de inteiros:\n");
-    scanf("%i",&N);
-    int v[N];
-    printf("Digite o valor de cada elemento, separando por espaco:\n");
-    for(i = 0; i < N; i++)
-    {
-        scanf(" %i",&v[i]);
-    }
-    maximoMinimo(v, N, &maximo, &minimo);
-    printf("\nO elemento maximo do vetor eh: %i\n", maximo);
-    printf("O elemento minimo do vetor eh: %i\n", minimo);
-    return 0;
-}
 void maximoMinimo(int *v, int N, int *maximo, int *minimo)
 {
     int i, j,temporaria;
