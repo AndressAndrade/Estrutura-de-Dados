e2bec172332ae198b7430109dc86b2ea465da161
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-09-30 10:08:18 -0300

    Exercicio 11

---
+++ Lista1/exer11.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+    int N,i,maior;
+    void omaior(int *v, int N, int *maior);
+    printf("Digite o numero de elementos de seu vetor de inteiros:\n");
+    scanf("%i",&N);
+    int v[N];
+    printf("Digite o valor de cada elemento, separando por espaco:\n");
+    for(i = 0; i < N; i++)
+        {
+            scanf(" %i",&v[i]);
+        }
+    omaior(v, N, &maior);
+    printf("O elemento maior do vetor eh: %i\n", maior);
+    return 0;
+}
+
+void omaior(int *v, int N, int *maior)
+{
+    int i, j,temporaria;
+    for(i=0; i < N; ++i)
+        {
+            for(j= i + 1; j < N; ++j)
+                {
+                    if(v[i] > v[j])
+                        {
+                            temporaria = v[i];
+                            v[i] = v[j];
+                            v[j] = temporaria;
+                        }
+                }
+        }
+    *maior = v[N-1];
+
+}
