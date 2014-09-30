d1d705bacebbf36fd1b8d5d0a6bc0c17903a66b8
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-09-30 15:44:35 -0300

    Exercise 9 Parcial

---
+++ Lista1/exe9.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+        int N,M;
+        printf("Digite o numero de elementos de seu primeiro vetor");
+        scanf("%i",&N)
+        printf("Digite o numero de elementos de seu segundo vetor");
+        scanf("%i",&M)
+}
