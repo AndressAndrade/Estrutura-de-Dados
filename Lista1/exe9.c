a5d4a20d8e05ecbbdd69a1a4b25c971f1608e4a5
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-09-30 16:09:07 -0300

    Update exe9.c

--- Lista1/exe9.c
+++ Lista1/exe9.c
@@ -2,9 +2,25 @@
 
 int main()
 {
-        int N,M;
-        printf("Digite o numero de elementos de seu primeiro vetor");
-        scanf("%i",&N)
-        printf("Digite o numero de elementos de seu segundo vetor");
+        int N,M,x,*resultado;
+        int somadevetores(int N, int M, int *v1, int *v2)
+        printf("Digite o numero de elementos de seu primeiro vetor:\n");
+        scanf("%i",&N);
+        int v1[N];
+        printf("Digite os elementos separando por espaco:\n");
+        for(x = 0; x < N; x++)
+        {
+            scanf("%i",&v1[i]);
+        }
+        printf("Digite o numero de elementos de seu segundo vetor: \n");
         scanf("%i",&M)
+        int v2[m];
+        for(x = 0; x < M; x++)
+        {
+            scanf("%i",&v2[i]);
+        }
+        printf("A soma dos dois vetores eh:",resultado)
+        
 }
+
+int somadevetores(int N, int M, int *v1, int *v2)
