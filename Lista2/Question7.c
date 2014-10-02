5874e7e1b8066bab2e85ae3ac8b7f0ea144870c7
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-10-02 13:48:30 -0300

    Questao 7

--- Lista2/Question7.c
+++ Lista2/Question7.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char palavra1[20] = "Andressa";
+    char palavra2[20];
+    strcpy(palavra2, palavra1);
+    printf("strcpy(palavra1, palavra2) :  %s\n", palavra2 );
+    return 0;
+
+}
