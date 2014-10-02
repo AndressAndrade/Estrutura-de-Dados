6caadcd8909e7421311181f51428c5f4166c48b7
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-10-02 11:59:38 -0300

    Questao 21

---
+++ Lista1/exer21.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void verificar(char *a, char *b)
+{
+    int x = 0;
+    int y = 0;
+    for(x ; x < 8; x++)
+    {
+        for(y ; y < 19; y++)
+        {
+            if(*b != *a)
+                y += 1;
+
+
+        }
+        x += 1;
+    }
+
+
+
+
+}
+
+int main()
+{
+    char v1[] = "Andressa";
+    char v2[] = "Bem-Vinda, Andressa";
+    char *x = v1;
+    char *y = v2;
+    verificar(*x,*y);
+    return 0;
+
+}
