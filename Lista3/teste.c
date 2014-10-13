33ad929a013f800fd78b1da8c34c72cd3c573f75
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-10-12 21:32:41 -0300

    Questao 1

--- Lista3/teste.c
+++ Lista3/teste.c
@@ -1,1 +1,38 @@
 #include <stdio.h>
+#include<stdlib.h>
+
+
+int *aloca_vetor(int n)
+{
+    int *vetor;
+    vetor = malloc(n*sizeof(int));
+    return vetor;
+}
+
+void imprime(int *vector,int n)
+{
+    int i;
+    printf("Digite os valores de seu vetor, separando por espaco:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf(" %i",&vector[i]);
+    }
+    printf("\nO vetor lido foi:\n\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%i ",vector[i]);
+    }
+    printf("\n");
+
+}
+
+int main()
+{
+    int x,*vetor;
+    printf("Digite o tamanho do vetor para alocacao:\n");
+    scanf("%i",&x);
+    vetor = aloca_vetor(x);
+    imprime(vetor,x);
+    return 0;
+
+}
