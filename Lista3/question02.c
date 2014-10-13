e7bcc5a6ad062c4d100807215ae6c9d9e54c2edb
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-10-12 22:15:07 -0300

    Questao 2

---
+++ Lista3/question02.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n = 1;
+    int i = 0;
+    int c = 0;
+    int *vetor;
+
+    while(n > 0)
+    {
+        printf("Digite um valor:\n");
+        scanf("%i",&n);
+        vetor[i] = n;
+        i++;
+        c++;
+    }
+    printf("\nO vetor lido foi:\n\n");
+    for(i = 0; i < c; i++)
+    {
+        printf("%i ",vetor[i]);
+    }
+    return 0;
+
+}
