183fa744fa85e8f4de97b85d1f18ba7fd3de9bc0
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-10-14 00:33:58 -0300

    Questao 5

---
+++ Lista3/questao05.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    char nome[30];
+    char sobrenome[30];
+    int matricula;
+    int anonasc;
+
+}alunos;
+
+int main()
+{
+    int i,n;
+
+    printf("Informe a quantidade de alunos:\n");
+    scanf("%i",&n);
+
+    alunos *p;
+
+    p = (alunos*)malloc(sizeof(alunos));
+
+
+    for(i = 0; i < n; i++)
+    {
+        fflush(stdin);
+        printf("\nNome do Aluno %d: ", i+1);
+        gets(p->nome);
+
+        printf("Sobrenome: ");
+        gets(p->sobrenome);
+
+        printf("Numero de Matricula: ");
+        scanf("%i", &p->matricula);
+
+        printf("Ano de Nascimento: ");
+        scanf("%i", &p->anonasc);
+
+        p=p+1;
+
+    }
+    p = p - n;
+    printf("\nExibindo Cadastros:\n");
+
+    for(i = 0 ; i < n; i++)
+    {
+        printf("\nAluno %d\n", i+1);
+        printf("Nome: %s\n",(p[i]).nome);
+        printf("Sobrenome: %s\n", (p[i]).sobrenome);
+        printf("Matricula: %i\n", (p[i]).matricula);
+        printf("Ano de Nascimento: %i\n", (p[i]).anonasc);
+
+    }
+    free(p);
+    return 0;
+}
