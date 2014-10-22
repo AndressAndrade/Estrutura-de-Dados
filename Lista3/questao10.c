63d20fb436a79a029fe1a3f5ef00b311618257b9
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-10-22 19:40:16 -0300

    Questao 10

--- Lista3/questao10.c
+++ Lista3/questao10.c
@@ -2,31 +2,61 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main()
+int *alocar_vetor(int x)
 {
-     int i,k,x,c = 1;
-     srand(time(NULL));
-     printf("Qual numero de elementos deseja no vetor: ");
-     scanf("%i",&k);
-     int v[k];
-     printf("Qual valor deseja procurar multiplo: ");
-     scanf("%i",&x);
-     printf("\n\nGerando %i valores aleatorios:\n\n",k);
+    int *vetor;
 
-     for (i = 0; i < k; i++)
-     {
-           v[i] = rand() % 100;
-           printf("%i ", v[i]);
-     }
+    vetor = malloc(x*sizeof(int)) ;
 
-     printf("\n\nProcurando multiplos...\n\n");
-     for (i = 0; i < k; i++)
-     {
-           if(v[i] % x == 0)
+    return vetor;
+
+}
+
+void preenchexibe_vetor(int *vetor, int x)
+{
+    int i;
+    srand(time(NULL));
+    for (i = 0; i < x; i++)
+    {
+        vetor[i] = rand() % 100;
+        printf("%i ", vetor[i]);
+    }
+    printf("\n");
+}
+
+void multiplo_elemento(int *vetor, int a,int b)
+{
+    int i,c = 1;
+    printf("\n\nProcurando multiplos...\n\n");
+    for(i = 0; i < a; i++)
+    {
+           if(vetor[i] % b == 0)
            {
-               printf("Temos o multiplo %i: %i\n",c,v[i]);
+               printf("Temos o multiplo %i: %i\n",c,vetor[i]);
                c++;
            }
     }
+
+}
+
+
+int main()
+{
+     int k,x;
+     int *vector;
+
+     printf("Qual numero de elementos deseja no vetor: ");
+     scanf("%i",&k);
+     vector = alocar_vetor(k);
+
+     printf("\nGerando %i valores aleatorios:\n\n",k);
+     preenchexibe_vetor(vector, k);
+
+
+     printf("\nQual valor deseja procurar multiplo: ");
+     scanf("%i",&x);
+     multiplo_elemento(vector, k,x);
+
+    free(vector);
     return 0;
 }
