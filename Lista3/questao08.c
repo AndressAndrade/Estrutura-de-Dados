dc9609bc99887e0407ea03c727e1407ec4b0c38d
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-10-23 13:00:59 -0300

    Questao 8

--- Lista3/questao08.c
+++ Lista3/questao08.c
@@ -1,5 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+int **alocar_matriz(int linhas, int colunas)// ALOCA MATRIZ
+{
+    int **matriz;
+
+    int i;
+
+    matriz = malloc(linhas*sizeof(int*));
+
+    for(i = 0 ; i < linhas; i++)
+    {
+        matriz[i] = malloc(colunas*sizeof(int));
+    }
+
+    return matriz;
+
+}
+
+void preencher_matriz(int**matriz, int linhas, int colunas)//PREENCHE E EXIBE MATRIZ
+{
+    int i,j;
+    srand(time(NULL));
+
+        for(i = 0; i < linhas; i++)
+        {
+            for(j = 0; j < colunas; j++)
+            {
+                matriz[i][j] = rand() % 100;
+                printf("%i ",matriz[i][j]);
+
+            }
+            printf("\n");
+        }
+    printf("\n");
+}
+
 
 int encontra_valor(int **matriz, int linhas,int colunas, int x)
 {
@@ -12,6 +49,7 @@ int encontra_valor(int **matriz, int linhas,int colunas, int x)
             if(matriz[i][j] == x)
             {
                 teste = 1;
+                break;
             }
         }
     }
@@ -22,14 +60,15 @@ int encontra_valor(int **matriz, int linhas,int colunas, int x)
     else
     {
         return 0;
-
     }
+
+
 }
 
 
 int main()
 {
-    int **matriz,i,j,x,linhas,colunas;
+    int **matriz,x,y,linhas,colunas;
 
     printf("Digite o numero de linhas da matriz: ");
     scanf("%i",&linhas);
@@ -37,26 +76,15 @@ int main()
     scanf("%i",&colunas);
     printf("\n");
 
-    matriz = malloc(colunas*sizeof(int*));
+    matriz =  alocar_matriz(linhas,colunas);
+    preencher_matriz(matriz,linhas,colunas);
 
-    for(i = 0; i < linhas; i++)
-    {
-        matriz[i] = malloc(linhas*sizeof(int));
-    }
-
-    for(i = 0; i < linhas; i++)
-    {
-        for(j = 0; j < colunas; j++)
-        {
-            printf("Digite a matriz[%i][%i]: ",i+1,j+1);
-            scanf("%i", &matriz[i][j]);
-        }
-    }
     printf("\nDigite um valor: ");
     scanf("%i",&x);
 
-    encontra_valor(matriz, linhas, colunas, x);
+    y = encontra_valor(matriz, linhas, colunas, x);
+
+    return y;
 
     free(matriz);
-
 }
