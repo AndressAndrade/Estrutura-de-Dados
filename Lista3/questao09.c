7bd3afcaad98917cd1e02da5c26d0d6c2eb0fa4e
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-10-23 11:45:15 -0300

    Questao 9

--- Lista3/questao09.c
+++ Lista3/questao09.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float **alocar_matriz(int linhas, int colunas)// ALOCA MATRIZ
+{
+    float **matriz;
+
+    int i;
+
+    matriz = malloc(linhas*sizeof(float*));
+
+    for(i = 0 ; i < linhas; i++)
+    {
+        matriz[i] = malloc(colunas*sizeof(float));
+    }
+
+    return matriz;
+
+}
+
+void preencher_matriz(float**matriz, int linhas, int colunas)//PREENCHE MATRIZ
+{
+    int i,j;
+
+        for(i = 0; i < linhas; i++)
+        {
+            for(j = 0; j < colunas; j++)
+            {
+                  printf("Digite a matriz[%i][%i]: ",i+1,j+1);
+                  scanf("%f", &matriz[i][j]);
+            }
+        }
+    printf("\n");
+}
+
+void matriz_org(float **matriz, int linhas, int colunas)//DEVOLVE MATRIZ NO LAYOUT APROPRIADO
+{
+    int i,j;
+
+    for(i = 0; i < linhas; i++)
+    {
+        for(j = 0; j < colunas; j++)
+        {
+            printf("%.2f ",matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 int main()
 {
@@ -13,35 +58,14 @@ int main()
     scanf("%i",&colunas);
     printf("\n");
 
-    matriz = malloc(colunas*sizeof(float*)); //ALOCA MATRIZ
+    matriz = alocar_matriz(linhas,colunas);
+    preencher_matriz(matriz,linhas,colunas);
 
-    for(i = 0; i < linhas; i++)
-    {
-        matriz[i] = malloc(linhas*sizeof(float));
-    }
-
-    for(i = 0; i < linhas; i++)  //PREENCHE MATRIZ
-    {
-        for(j = 0; j < colunas; j++)
-        {
-            printf("Digite a matriz[%i][%i]: ",i+1,j+1);
-            scanf("%f", &matriz[i][j]);
-        }
-    }
 
     printf("\nMatriz Organizada:\n\n");
 
-    for(i = 0; i < linhas; i++)  //DEVOLVE MATRIZ COM LAYOUT APROPRIADO
-    {
-        for(j = 0; j < colunas; j++)
-        {
-            printf("%.2f ",matriz[i][j]);
-        }
-        printf("\n");
-    }
-
+    matriz_org(matriz,linhas,colunas);
 
     free(matriz);
     return 0;
-
 }
