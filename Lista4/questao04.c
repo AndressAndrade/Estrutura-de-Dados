8368f782087e64ab305f5da579dd6a91bbf4ddf5
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-10-26 18:48:41 -0300

    Questao 4

--- Lista4/questao04.c
+++ Lista4/questao04.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int *alocar_espaco()
 {
-    int *p,i,j,k;
-    int matriz[2][4];
+    int *p;
+
     p = malloc(40*sizeof(int));
 
+    return;
+}
+
+void preenche_matriz(int *p)
+{
+    int k,i,j;
+    int matriz[2][4];
     for(k = 0; k < 5; k++)
     {
         printf("Preencha a matriz de inteiros %i:\n",k+1);
@@ -24,14 +31,26 @@ int main()
 
     }
     p -= 40;
+}
 
-
+void test(int *p)
+{
+    int i;
     for(i = 0;i < 40;i++)// teste
     {
         printf("%i ",*p);
         p++;
     }
 
+}
+int main()
+{
+    int *p;
+    p = alocar_espaco();
+
+    preenche_matriz(p);
+
+    test(p);
 
     free(p);
     return 0;
