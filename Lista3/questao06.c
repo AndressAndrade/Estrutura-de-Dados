47d842cee742ef30b572f1d3e8015595a1efa0d1
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-10-26 17:15:02 -0300

    Questao 6

--- Lista3/questao06.c
+++ Lista3/questao06.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
+char *alocar_palavra(palavra)
 {
-  int i;
-  int count = 0;
-  int z = 0;
+    palavra = (char*)malloc(sizeof(char));
 
-  char *palavra;
-  palavra = (char*)malloc(sizeof(char));
+    return palavra;
+}
 
-  fflush(stdin);
-  printf("Digite uma palavra, em minusculo: ");
-  gets(palavra);
+int remove_vogal(char *palavra)
+{
+    int i;
+    int count = 0;
+    int z = 0;
 
-  while(palavra[z] != '\0')
-  {
+    while(palavra[z] != '\0')
+    {
     if(palavra[z] == 'a')
     {
       palavra[z] = ' ';
@@ -53,12 +53,33 @@ int main()
         z++;
     }
   }
+  return count;
+}
 
-  for(i = 0; i < count; i++)
-  {
+void imprime_semvogal(char *palavra)
+{
+    int i,k;
+    k = remove_vogal(palavra);
+    for(i = 0; i < k; i++)
+    {
       printf("%c",palavra[i]);
-  }
-  printf("\n");
+    }
+    printf("\n");
+}
+
+
+int main()
+{
+
+  char *palavra;
+  palavra = alocar_palavra(palavra);
+
+  fflush(stdin);
+  printf("Digite uma palavra, em minusculo: ");
+  gets(palavra);
+
+  imprime_semvogal(palavra);
+
   free(palavra);
   return 0;
 }
